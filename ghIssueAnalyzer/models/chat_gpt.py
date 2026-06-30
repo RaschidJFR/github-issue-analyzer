9d4358a357b48ab3e1476092d8dab32b36f8e361
Raschid Jimenez <raschid.jimenez@mongodb.com>
2026-06-30 18:28:36 -0400

    feat: implement tracing with `mlflow`

--- ghIssueAnalyzer/models/chat_gpt.py
+++ ghIssueAnalyzer/models/chat_gpt.py
@@ -1,6 +1,14 @@
 from .interfaces import ConversationalLLM
 from openai import OpenAI
 
+# Use MLFlow to track the LLM usage
+# See https://mlflow.org/docs/latest/genai/tracing/quickstart/
+import mlflow
+mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_experiment("My Application")
+mlflow.openai.autolog()
+
+
 class ChatGPT(ConversationalLLM):
   def __init__(self, api_key:str, model='gpt-5-mini') -> None:
     self._client = OpenAI(api_key=api_key)
